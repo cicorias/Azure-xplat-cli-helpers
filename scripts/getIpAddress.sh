1cbc4eafac7922fbdbf9c2f7c980b7676e4be252
Shawn Cicoria <shawn@cicoria.com>
2015-06-26 18:59:29 -0400

    minor update

--- scripts/getIpAddress.sh
+++ scripts/getIpAddress.sh
@@ -8,20 +8,17 @@ source ./subscription.private
 path="/services/hostedservices/jettestarmpaas/deploymentslots/production"
 path2="/services/hostedservices/jettestarmpaas/deployments/8dfb0cde0c974faf99067af97b776b83"
 
+
 fullUrl="$urlHost/$sub$path" 
 fullUrl2="$urlHost/$sub$path2"
 
 
-echo $fullUrl
+curl --cert azure.pem:password --header x-ms-version:2012-03-01 $fullUrl -o deployments.xml
 
-#echo $fullUrl2
+sed -e "s/xmlns/ignore/" deployments.xml > deploymentsclean.xml
 
-curl --cert azure.pem:password --header x-ms-version:2012-03-01 $fullUrl -o out.xml
 
-sed -e "s/xmlns/ignore/" out.xml > outclean.xml
+#alternate but direct way - requires deployment ID/name
+curl --cert azure.pem:password --header x-ms-version:2012-03-01 $fullUrl2 -o out2.xml
 
-#curl --cert azure.pem:password --header x-ms-version:2012-03-01 $fullUrl2 -o out2.xml
-
-#xmllint --xpath 'string(/Deployment/RoleInstanceList/RoleInstance/IpAddress)' outclean.xml
-
-xmllint --xpath '/Deployment/RoleInstanceList/RoleInstance/IpAddress' outclean.xml
+xmllint --xpath '/Deployment/RoleInstanceList/RoleInstance/IpAddress' deploymentsclean.xml
